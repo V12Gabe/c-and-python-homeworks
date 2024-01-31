6d495213e6b940dc2b4a63cac4df158b32b6f292
V12Gabe <101881366+V12Gabe@users.noreply.github.com>
2024-01-31 09:53:52 +0100

    Homework 5 (incomplete)

--- feladat.py
+++ feladat.py
@@ -2,51 +2,57 @@
 # Neptun: CNFYPH
 # h: h162846
 
-
-class Palack:
-    def __init__(self, ital, max_urtartalom, jelenlegi_urtartalom=1):
-        self.ital = ital
-        self.max_urtartalom = max_urtartalom
-        self._jelenlegi_urtartalom = jelenlegi_urtartalom
-
-    @property
-    def jelenlegi_urtartalom(self):
-        return self._jelenlegi_urtartalom
-
-    @jelenlegi_urtartalom.setter
-    def jelenlegi_urtartalom(self, ertek):
-        if ertek > self.max_urtartalom:
-            self._jelenlegi_urtartalom = self.max_urtartalom
-        else:
-            self._jelenlegi_urtartalom = ertek
-
-        if ertek == 0:
-            self.ital = None
-
-    def suly(self):
-        palacksuly = self.max_urtartalom/35 + self._jelenlegi_urtartalom
-        return palacksuly
-
-    def __str__(self):
-        return f"Palack, benne levo ital: {self.ital}, jelenleg {self._jelenlegi_urtartalom} ml van benne, maximum {self.max_urtartalom} ml fer bele."
-
-    def __eq__(self, other):
-        if self.ital == other.ital and self.max_urtartalom == other.max_urtartalom and self._jelenlegi_urtartalom == other._jelenlegi_urtartalom:
-            return True
-        else:
-            return False
-
-    def __iadd__(self, other):
-        self._jelenlegi_urtartalom += other._jelenlegi_urtartalom
+#def hogolyo_csata(lista):
+#    szotar = {}
+#    for i in lista:
+#        for kulcs, ertek in i.items():
+#            if "fejtalalat" not in szotar[kulcs]:
+#                szotar[kulcs]["fejtalalat"] = 0
+#            szotar[kulcs] = ertek
 
 
-class VisszavalthatoPalack(Palack):
-    def __init__(self, ital, max_urtartalom, jelenlegi_urtartalom=1, palackdij=25):
-        super().__init__(ital, max_urtartalom, jelenlegi_urtartalom)
-        self.palackdij = palackdij
-
-    def __str__(self):
-        return f"VisszavalthatoPalack, benne levo ital: {self.ital}, jelenleg {self.jelenlegi_urtartalom} ml van benne, maximum {self.max_urtartalom} ml fer bele."
 
 
+
+
+
+#    return szotar
+
+
+def hogolyo_pontossag(szotar1):
+    szotar2 = szotar1
+    for kulcs, ertek in szotar1.items():
+        szotar2[kulcs]["pontossag"] = szotar1[kulcs]["talalt"]/szotar1[kulcs]["eldobott_hogolyok"]
+
+    return szotar2
+
+
+def hogolyo_piros_lap(szotar):
+    lista = []
+    for kulcs, ertek in szotar.items():
+        if szotar[kulcs]['pontossag'] >= 0.7 and (szotar[kulcs]['fejtalalat']/szotar[kulcs]['talalt']) >= 0.5:
+            lista.append(kulcs)
+    return lista
+
+
+tesztszotar = {
+    "Tamas": {
+        "eldobott_hogolyok": 6,
+        "talalt": 3,
+        "fejtalalat": 0
+    },
+    "Ferenc": {
+        "eldobott_hogolyok": 19,
+        "talalt": 8,
+        "fejtalalat": 2
+    },
+    "Csaba": {
+        "eldobott_hogolyok": 32,
+        "talalt": 2,
+        "fejtalalat": 1
+    }
+}
+
+
+#hogolyo_pontossag(tesztszotar)
 
